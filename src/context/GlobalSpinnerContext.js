18acd1fc11aca565fca54694269650bce871da89
Justin <4635401+kerj@users.noreply.github.com>
2021-01-12 14:27:20 -0800

    useMemo prevents recreating the context and rerendering all consumers

--- src/context/GlobalSpinnerContext.js
+++ src/context/GlobalSpinnerContext.js
@@ -1,11 +1,16 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useState, useMemo } from 'react'
 
 export const GlobalSpinnerContext = createContext();
 
 export const GlobalSpinnerContextProvider = props => {
   const [isLoading, setIsLoading] = useState(false)
+
+  const context = useMemo(() => ({
+    isLoading,
+    setIsLoading,
+  }), [isLoading])
   return (
-    <GlobalSpinnerContext.Provider value={{isLoading, setIsLoading}}>
+    <GlobalSpinnerContext.Provider value={context}>
       {props.children}
     </GlobalSpinnerContext.Provider>
   )
