11ca5c1806fe6a497ecb7f2fa7614ec26490c420
Justin <4635401+kerj@users.noreply.github.com>
2021-01-12 14:53:20 -0800

    add check of context value

--- src/context/GlobalSpinnerContext.js
+++ src/context/GlobalSpinnerContext.js
@@ -2,10 +2,23 @@ import React, { createContext, useState, useContext } from 'react'
 
 const GlobalSpinnerContext = createContext();
 const GlobalSpinerActionsContext = createContext();
-// place useContext here to consume context cleaner throughout app, see previous commit for diff
-export const useGlobalSpinnerContext = () => useContext(GlobalSpinnerContext);
-export const useGlobalSpinnerContextActions = () => useContext(GlobalSpinerActionsContext);
- 
+// check that context is accessed within provides, throw error otherwise
+export const useGlobalSpinnerContext = () => {
+  const context = useContext(GlobalSpinnerContext);
+  if (context === undefined) {
+    throw new Error('useGlobalSpinnerContext must called be within GlobalSpinnerContext provider')
+  }
+  return context;
+}
+
+export const useGlobalSpinnerContextActions = () => {
+  const context = useContext(GlobalSpinerActionsContext);
+  if (context === undefined) {
+    throw new Error('useGlobalSpinnerContextActions must called be within GlobalSpinerActionsContext provider')
+  }
+  return context;
+}
+
 export const GlobalSpinnerContextProvider = props => {
   const [isLoading, setIsLoading] = useState(false)
 
